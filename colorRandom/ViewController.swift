cbdc8fc8c39ebff72ac98927ec2bacd7eadd585e
DanDan <aass65547@gmail.com>
2022-08-30 20:32:15 +0800

    Initial Commit

---
+++ colorRandom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  colorRandom
+//
+//  Created by 邱達瑋 on 2022/8/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
